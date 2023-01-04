ae5b61b1f67530845cc2a54bc425580f67917845
Mr. Kiedes <akiedes@vtsd.com>
2023-01-04 13:43:45 -0500

    Lab Update

--- Lab11avst.java
+++ Lab11avst.java
@@ -22,6 +22,18 @@ public class Lab11avst
     public static void computePrimes(boolean primes[])
     {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
+        // Step 1: Treat all numbers as Prime
+        // Write a loop that changes ALL numbers to prime (true) in the array.
+        for(int k = 2; k < primes.length; k++) {
+            // Change all the values to true here
+
+        }
+
+        // Step 2: This is where you remove the primes
+        // To start, JUST remove the multiples of 2
+        for(int k = 2; k < primes.length; k++) {
+
+        }
 
     }
 
@@ -29,6 +41,12 @@ public class Lab11avst
     {
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
+
+        for(int k = 2; k < primes.length; k++){
+            if(){ // Write the Condition yourself
+                System.out.print(k + " ");
+            }
+        }
 
     }
 
